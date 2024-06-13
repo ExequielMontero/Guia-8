c8a6e12b53dc01f1539a4193912f166e03319b63
ExequielMontero <exeee2015@gmail.com>
2024-06-13 00:59:49 -0300

    Subiendo todo

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static bool esono = false;
+        static int res;
+        static int i = 0;
+        static int[] vector1 = new int[100];
+        static bool bandera;
+        static void Main(string[] args)
+        {
+            int opcion, num;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Guardar numeros");
+                Console.WriteLine("2 - Ordenar lista");
+                Console.WriteLine("3 - Metodo Secuencial");
+                Console.WriteLine("4 - Metodo Binario");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            {
+
+                                do
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Ingrese el numero que quiere guardar o [0] para volver");
+                                    num = Convert.ToInt32(Console.ReadLine());
+                                    if (num != 0)
+                                    {
+                                        guardarnumeros(num);
+                                    }
+                                } while (num != 0);
+                            }
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Clear();
+                            MetodoBurbuja();
+                            Console.WriteLine("¡Lista Ordenada con exito!");
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ingrese el numero que quiere buscar");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            busquedasecuencial(num);
+                            Console.Clear();
+                            if (esono)
+                            {
+                                Console.WriteLine($"El numero buscado se encuentra en la posicion {res+1} de la lista");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se encontro el numero buscado en la lista");
+                            }
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ingrese el numero que quiere buscar");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            int buscado = metodobinario(num);
+                            if (bandera)
+                            {
+                                Console.WriteLine($"El numero buscado se encuentra en la posicion {buscado + 1} de la lista");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se encontro el numero buscado en la lista");
+                            }
+                        }
+                        break;
+                }
+                Console.ReadKey();
+            } while (opcion != 0);
+
+
+        }
+
+        static void guardarnumeros(int num)
+        {
+
+            vector1[i] = num;
+
+            i++;
+        }
+
+        static void MetodoBurbuja()
+        {
+            for (int a = 0; a < i - 1; a++)
+            {
+                for (int n = a + 1; n < i; n++)
+                {
+                    if (vector1[a] > vector1[n])
+                    {
+                        int aux = vector1[n];
+                        vector1[n] = vector1[a];
+                        vector1[a] = aux;
+                    }
+                }
+            }
+
+        }
+
+        #region con metodo secuencia con for
+        static void busquedasecuencial(int num)
+        {
+            esono = false;
+            for (int h = 0; h < i; h++)
+            {
+                if (vector1[h] == num)
+                {
+                    esono = true;
+                    res = h;
+                }
+            }
+
+        }
+        #endregion fin con metodo secuencial for
+
+        static int metodobinario(int num)
+        {
+            int izq = 0, medio , der, pos = -1;
+            bandera = false;
+            der = i - 1;
+            do
+            {
+                medio = (izq + der) / 2;
+                if (vector1[medio] == num)
+                {
+                    bandera = true;
+                }
+                else if (vector1[medio] < num)
+                {
+                    izq = medio + 1;
+                }
+                else
+                {
+                    der = medio - 1;
+                }
+            }while(( izq <= der) && (!bandera));
+
+            if (bandera)
+            {
+                pos = medio;
+            }
+            return pos;
+        }
+
+
+    }
+}
---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static double promedio;
+        static int i = 0, j=0;
+        static double[] notas = new double[100];
+        static int[] libreta = new int[100];
+        static string[] nombre = new string[100];
+        static double[] mayornotas = new double[100];
+        static int[] mayorlibreta = new int[100];
+        static string[] mayornombre = new string[100];
+        static void Main(string[] args)
+        {
+            int opcion;
+            string alumno;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Ingrese el nombre del alumno {i+1}  - - [0] Para terminar de cargar");
+                alumno = Console.ReadLine();
+                if (alumno != "0")
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Ahora ingrese el numero de libreta del alumno {i+1}");
+                    int lib = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine($"Por ultimo ingrese la nota del alumno {i+1}");
+                    int not = Convert.ToInt32(Console.ReadLine());
+                    guardarnombre(alumno);
+                    guardarlibreta(lib);
+                    guardarnota(not);
+                    i++;
+                }
+            } while (alumno != "0");
+                
+            
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Calcular Promedio");
+                Console.WriteLine("2 - Alumnos Mayor Al Promedio");
+                Console.WriteLine("3 - Buscar Alumno");
+                Console.WriteLine("4 - Dentro De Lista Mayor Promedio (Alumno Mayor Promedio - Alumno Menor Promedio)");
+                Console.WriteLine("0 - Salir/Otros");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            CalcularPromedio();
+                            Console.WriteLine($"El promedio de nota de los alumnos es de {promedio} ");
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Clear();
+                            if (j == 0)
+                            {
+                                CalcularPromedio();
+                                alumnosmayor();
+                                ordenamientoburbuja();
+                            }
+
+                            for(int l = j-1; l>-1; l--)
+                            {
+                                Console.WriteLine($"- Alumno: {mayornombre[l]}");
+                                Console.WriteLine($"- Numero Libreta: {mayorlibreta[l]}");
+                                Console.WriteLine($"- Nota: {mayornotas[l]}");
+                                Console.WriteLine(""); 
+                                Console.WriteLine("");
+                            }
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Libreta del alumno que quiere buscar");
+                            int lib = Convert.ToInt32(Console.ReadLine());
+                            int posicion = buscaralumno(lib);
+                            if (posicion != -1)
+                            {
+                                Console.Clear();
+                                Console.WriteLine($"- Alumno: {nombre[posicion]}");
+                                Console.WriteLine($"- Numero Libreta: {libreta[posicion]}");
+                                Console.WriteLine($"- Nota: {notas[posicion]}");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("¡Alumno no encontrado!");
+                            }
+                           
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.Clear();
+                            if (j == 0)
+                            {
+                                CalcularPromedio();
+                                alumnosmayor();
+                                ordenamientoburbuja();
+                            }
+                            Console.WriteLine($"- Alumno: {mayornombre[j-1]}");
+                            Console.WriteLine($"- Numero Libreta: {mayorlibreta[j-1]}");
+                            Console.WriteLine($"- Nota: {mayornotas[j-1]}");
+                            Console.WriteLine("");
+                            Console.WriteLine("------------------------------------------------------------");
+                            Console.WriteLine($"- Alumno: {mayornombre[0]}");
+                            Console.WriteLine($"- Numero Libreta: {mayorlibreta[0]}");
+                            Console.WriteLine($"- Nota: {mayornotas[0]}");
+
+                        }
+                        break;
+                }
+                if (opcion != 0)
+                {
+                    Console.ReadKey();
+                }
+            } while (opcion != 0);
+        }
+
+        static void guardarnombre(string alumno)
+        {
+            nombre[i] = alumno;
+        }
+        static void guardarlibreta(int nmrolibreta)
+        {
+            libreta[i] = nmrolibreta;
+        }
+        static void guardarnota(int nota)
+        {
+            notas[i] = nota;
+        }
+
+        static void CalcularPromedio()
+        {
+            double sum=0;
+            for(int h=0; h<i; h++)
+            {
+                sum += notas[h];
+            }
+
+            promedio = sum / i - 1;
+
+        }
+
+        static void alumnosmayor()
+        {
+            for(int h=0; h<i; h++)
+            {
+                if (notas[h] > promedio)
+                {
+                    mayorlibreta[j] = libreta[h];
+                    mayornombre[j] = nombre[h];
+                    mayornotas[j] = notas[h];
+                    j++;
+                }
+            }
+        }
+
+        static void ordenamientoburbuja()
+        {
+            for(int a=0; a<j-1; a++)
+            { 
+                for(int l=a+1; l<j; l++)
+                {
+                    if (mayornotas[a] > mayornotas[l])
+                    {
+                        double aux = mayornotas[l];
+                        mayornotas[l] = mayornotas[a];
+                        mayornotas[a] = aux;
+
+                        string auxnom = mayornombre[l];
+                        mayornombre[l] = mayornombre[a];
+                        mayornombre[a] = auxnom;
+
+                        int auxlib = mayorlibreta[l];
+                        mayorlibreta[l] = mayorlibreta[a];
+                        mayorlibreta[a] = auxlib;
+
+                    }
+                }
+            }
+        }
+
+        
+        static int buscaralumno(int nmrolibreta)
+        {
+            int posicion = -1;
+            for(int l = 0; l<i; l++)
+            {
+                if (libreta[l] == nmrolibreta)
+                {
+                    posicion = l;
+                }
+            }
+
+            return posicion;
+        }
+
+    }
+}
